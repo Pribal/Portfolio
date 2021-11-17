368f98540badd18e045fdbed0f0af64f2c08c197
Pribal <oscar.giraudoux@gmail.com>
2021-11-17 18:45:37 +0100

    Horloge fonctionnelle

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-//Set Time and Date
-now =  new Date();
 
-//Time
 function Time() {
+	now =  new Date();
+
+	//Time
 	minutes = now.getMinutes();
 	hours = now.getHours();
 	document.getElementById("time").innerHTML = hours + ":" + minutes;
+
+	//Date
+	year = now.getFullYear();
+	month = now.getMonth() + 1;
+	day = now.getDate();
+	document.getElementById("date").innerHTML = day + "/" + month + "/" + year;
 }
 
 //Refresh Time
-setTimeout(Time, 1000);
-
-//Date
-year = now.getFullYear();
-month = now.getMonth() + 1;
-day = now.getDate();
-document.getElementById("date").innerHTML = day + "/" + month + "/" + year;
-
+Time();
+setInterval(Time, 10000);
 
 //Onload Popup
 function Onload() {
